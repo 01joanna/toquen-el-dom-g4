fb05bb35b6b2238f0ec1d014424ae1a887a198de
Arleny <arleny.ares@gmail.com>
2023-12-05 10:52:30 +0100

    Actualizacion video tutorial

--- test/sounds.test.js
+++ test/sounds.test.js
@@ -21,9 +21,3 @@ test('sounds.js - la funcion handleKeyPress deberia existir', () => {
 test('En el archivo sounds.js existe la funcion handleTubeTouchStart ', () => {
     expect(handleTubeTouchStart).to.be.a('function')
 });
-
-
-/*En el contexto de Chai, la sintaxis .to.be.a se utiliza para afirmar el tipo de un valor en JavaScript. En otras palabras, se espera que el valor al que se aplica esta afirmación sea del tipo especificado. 
-En esta afirmación .to.be.a, estamos diciendo que esperamos que playAudio o handleTubeTouchStart  sea una función. Si playAudio no es una función, la prueba fallará.
-
-*/
